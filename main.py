06e2ff2333b156f25f9fbd4f8f2dac9b930e345c
Haluk Ural <haluk8725@hotmail.com>
2023-04-30 11:59:47 +0300

    Task1 is done

---
+++ main.py
@@ -0,0 +1,18 @@
+
+## Reverse String Task
+
+# First option
+
+def reverseString1(reverse):
+    print("Reverse string first way:", reverse[::-1])
+
+#Second option
+def reverseString2(reverse):
+    str=""
+    for i in reverse:
+        str=i+str
+    print("Reverse string second way:",str)
+
+result=input("Write string:")
+reverseString1(result)
+reverseString2(result)
